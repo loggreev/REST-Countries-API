49a432cd0192eb47d234f94b72f6cc37df152a43
loggreev <70668891+loggreev@users.noreply.github.com>
2022-01-17 15:23:57 -0500

    fix for empty border countries list

--- src/components/BorderCountries.js
+++ src/components/BorderCountries.js
@@ -2,24 +2,29 @@ import './BorderCountries.css'
 
 function BorderCountries(props) {
     const borderCountries = props.data;
-    if (borderCountries.length > 0) {
-        const borderCountryItems = borderCountries.map(country =>
-            <div key={country} className="item element shadow">
-                {country}
-            </div>
-        );
-
-        return (
-            <div className="BorderCountries">
-                <p><b>Border Countries:</b></p>
-                <div className="flex-container">
-                    {borderCountryItems}
+    try {
+        if (borderCountries.length > 0) {
+            const borderCountryItems = borderCountries.map(country =>
+                <div key={country} className="item element shadow">
+                    {country}
                 </div>
-            </div>
-        );
+            );
+
+            return (
+                <div className="BorderCountries">
+                    <p><b>Border Countries:</b></p>
+                    <div className="flex-container">
+                        {borderCountryItems}
+                    </div>
+                </div>
+            );
+        }
+        else
+            return null;
     }
-    else
+    catch {
         return null;
+    }
 }
 
 export default BorderCountries;
